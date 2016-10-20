0a432001aca05e2fe15d3fd7753b89d44c3d07ea
glathoud <glat@glat.info>
2016-10-20 05:58:52 +0200

    loadWait logic

--- opinel.js
+++ opinel.js
@@ -88,7 +88,7 @@ function loadWait( /*string*/src, /*string | array*/vname, /*?object?*/obj, /*?i
 
 */
 {
-    if (!(loadWait[ src ]  &&  get( vname, obj )))
+    if (!loadWait[ src ])
     {
         loadWait[ src ] = 1;
         aC( document.head
